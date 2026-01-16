2648be49a19745aacf772a1fc53fb905d7ca4547
kadir-keskin-vinci <kadir.keskin@student.vinci.be>
2026-01-16 14:44:36 +0100

    refactor: extract notValid method

--- passwordChecker.ts
+++ passwordChecker.ts
@@ -1,10 +1,14 @@
 export class PasswordChecker{
     verif(input:string): boolean{
-        if(input.length < 8 || !/[!@#$%^&*(),.?":{}|<>]/.test(input) || !/[0-9]/.test(input) || input.toLowerCase().includes('ipl')) return false;
+        if(this.notValid(input)) return false;
        
     
 
         return true;
        
+    }
+
+    private notValid(input: string) {
+        return input.length < 8 || !/[!@#$%^&*(),.?":{}|<>]/.test(input) || !/[0-9]/.test(input) || input.toLowerCase().includes('ipl');
     }
 }
