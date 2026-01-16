ff2012de0ce30fdb3f02668e4f0fdfaa51a92db6
kadir-keskin-vinci <kadir.keskin@student.vinci.be>
2026-01-16 14:29:42 +0100

    add: verify return false when given don't contains special char

--- passwordChecker.test.ts
+++ passwordChecker.test.ts
@@ -3,9 +3,16 @@ import { PasswordChecker } from "./passwordChecker";
 describe("PasswordCheck", () =>{
    
     describe("verif method", () =>{
+
         it("should return false when given < 8", () =>{
             const passwordChecker = new PasswordChecker();
             const actual = passwordChecker.verif("lol");
+            expect(actual).toBe(false);
+        })
+
+        it("should return false when given  don't contains special char", () =>{
+            const passwordChecker = new PasswordChecker();
+            const actual = passwordChecker.verif("loloooooooo");
             expect(actual).toBe(false);
         })
       
--- passwordChecker.ts
+++ passwordChecker.ts
@@ -1,6 +1,8 @@
 export class PasswordChecker{
     verif(input:string): boolean{
         if(input.length < 8) return false;
+        if (!/[!@#$%^&*(),.?":{}|<>]/.test(input)) return false;
+    
 
         return true;
        
