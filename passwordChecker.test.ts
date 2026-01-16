a65e0000587c2ff8c7bfd8f428ab4447c8408ad7
kadir-keskin-vinci <kadir.keskin@student.vinci.be>
2026-01-16 14:32:00 +0100

    refactor: factorise PasswordChecker class instaction in tests

--- passwordChecker.test.ts
+++ passwordChecker.test.ts
@@ -1,17 +1,17 @@
 import { PasswordChecker } from "./passwordChecker";
 
 describe("PasswordCheck", () =>{
-   
+    
     describe("verif method", () =>{
-
+        const passwordChecker = new PasswordChecker();
         it("should return false when given < 8", () =>{
-            const passwordChecker = new PasswordChecker();
+            
             const actual = passwordChecker.verif("lol");
             expect(actual).toBe(false);
         })
 
         it("should return false when given  don't contains special char", () =>{
-            const passwordChecker = new PasswordChecker();
+            
             const actual = passwordChecker.verif("loloooooooo");
             expect(actual).toBe(false);
         })
